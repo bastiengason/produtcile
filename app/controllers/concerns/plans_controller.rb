8e7ba57219531fef820ff5e8076392d445e12166
Bastien Gason <bastiengason@hotmail.com>
2018-02-19 23:56:11 +0100

    change images size

---
+++ app/controllers/concerns/plans_controller.rb
@@ -0,0 +1,10 @@
+class PlansController < ApplicationController
+  def starter
+  end
+
+  def professional
+  end
+
+  def unlimited
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: 'application#home'
 
+  # Pages routes
   get '/blog', to: 'pages#blog'
+  get '/faq', to: 'pages#faq'
+
+  # Plans routes
+  get '/plan/starter', to: 'plans#starter'
+  get '/plan/professional', to: 'plans#professional'
+  get '/plan/unlimited', to: 'plans#unlimited'
 end
