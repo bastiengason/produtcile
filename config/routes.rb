b7e04c04fc902a4ffea54e362466139a5daf21de
Bastien Gason <bastiengason@hotmail.com>
2018-04-13 13:58:03 +0200

    add some pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/jobs', to: 'pages#jobs'
 
   # Plans routes
+  get '/plan', to: 'plans#plan'
   get '/plan/starter', to: 'plans#starter'
   get '/plan/professional', to: 'plans#professional'
   get '/plan/unlimited', to: 'plans#unlimited'
